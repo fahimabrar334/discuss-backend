9f6ba1e29760a8c99e1584434a69bd820ce82abc
Muhammad Fahim Abrar <116185841+fahimabrar334@users.noreply.github.com>
2026-07-01 02:36:52 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+
+// Firebase key from Render environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+module.exports = { db };
